32bc67b384808124661fb907f31c40e67b1f4009
liyin <liyincode@gmail.com>
2025-03-04 22:18:25 +0800

    feat: hmr

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import _ from 'lodash';
-import Print from './print';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.onclick = Print.bind(null, 'Hello webpack!');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+let element =  component();
+document.body.appendChild(element);
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log('Accepting the updated printMe module!');
+        document.body.removeChild(element);
+        element = component();
+        document.body.appendChild(element);
+    })
+}
